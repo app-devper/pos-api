517eb809d493db81bda298a235f741b39e69bd13
worawit <worawit.bs@gmail.com>
2024-02-15 22:22:24 +0700

    fix: remove product lot

--- app/featues/receive/usecase/delete_receive_item_by_lot_id.go
+++ app/featues/receive/usecase/delete_receive_item_by_lot_id.go
@@ -6,10 +6,20 @@ import (
 	"pos/app/domain/repository"
 )
 
-func DeleteReceiveItemByLotId(receiveEntity repository.IReceive) gin.HandlerFunc {
+func DeleteReceiveItemByLotId(receiveEntity repository.IReceive, productEntity repository.IProduct) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		id := ctx.Param("lotId")
 		result, err := receiveEntity.RemoveReceiveItemByLotId(id)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		lot, err := productEntity.RemoveProductLotById(id)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		_, err = productEntity.RemoveQuantityById(lot.ProductId.Hex(), lot.Quantity)
 		if err != nil {
 			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
